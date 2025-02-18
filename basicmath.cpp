3726b465cdb8479a594f001ea663b998472b57f9
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-18 21:02:50 +0000

    basic math in CPP

---
+++ basicmath.cpp
@@ -0,0 +1,16 @@
+// copyright (c) 2025 Tony G all rights reserved
+//
+// created by Tony G
+// created 2025 02 18
+// this is basic math in C++
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9 + 1 = " << (9 + 1) << std::endl;
+    std::cout << "9 * 2 = " << (9 * 2) << std::endl;
+    std::cout << "8 / 2 = " << (9 / 2) << std::endl;
+    std::cout << "2^3 = " << pow(2, 3) << std::endl;
+    std::cout << "Square root of 16 = " << sqrt(16) << std::endl;
+}
